dc8196691f8b30b3f78e907e871d2cd3d5cb7e8f
Austin Kirk <austin@Austins-MacBook-Pro.local>
2019-10-12 18:40:23 -0500

    added all positions and renamed

--- code.py
+++ code.py
@@ -5,6 +5,17 @@ import busio
 import time
 import eyes
 
+'''
+Eye Positions according to top left corner
+[x,x,x,x,x,x,x,x] x
+[x,x,0,1,2,x,x,x] 0
+[x,0,1,2,3,4,x,x] 1
+[x,0,1,2,3,4,x,x] 2
+[x,0,1,2,3,4,x,x] 3
+[x,x,0,1,x,x,x,x] 4
+[x,x,0,1,2,x,x,x] 5
+[x,x,x,x,x,x,x,x] x
+'''
 
 i2c = busio.I2C(board.SCL, board.SDA)
 eyes = eyes.Eyes(i2c)
@@ -12,27 +23,51 @@ eyes = eyes.Eyes(i2c)
 eyes.Brightness(3)
 
 while True:
-	eyes.Center()
-	time.sleep(1)
+	eyes.Pos0_0()
+	time.sleep(0.2)
+	eyes.Pos0_1()
+	time.sleep(0.2)
+	eyes.Pos0_2()
+	time.sleep(0.2)
 	
-	eyes.MidLeft()
-	time.sleep(1)
-	
-	eyes.Left()
-	time.sleep(1)
-	
-	eyes.MidLeft()
-	time.sleep(1)
-	
-	eyes.Center()
-	time.sleep(1)
-	
-	eyes.MidRight()
-	time.sleep(1)
-	
-	eyes.Right()
-	time.sleep(1)
+	eyes.Pos1_4()
+	time.sleep(0.2)
+	eyes.Pos1_3()
+	time.sleep(0.2)
+	eyes.Pos1_2()
+	time.sleep(0.2)
+	eyes.Pos1_1()
+	time.sleep(0.2)
+	eyes.Pos1_0()
+	time.sleep(0.2)
 
-	eyes.MidRight()
-	time.sleep(1)
+	eyes.Pos2_0()
+	time.sleep(0.2)
+	eyes.Pos2_1()
+	time.sleep(0.2)
+	eyes.Pos2_2()
+	time.sleep(0.2)
+	eyes.Pos2_3()
+	time.sleep(0.2)
+	eyes.Pos2_4()
+	time.sleep(0.2)
+
+	eyes.Pos3_4()
+	time.sleep(0.2)
+	eyes.Pos3_3()
+	time.sleep(0.2)
+	eyes.Pos3_2()
+	time.sleep(0.2)
+	eyes.Pos3_1()
+	time.sleep(0.2)
+	eyes.Pos3_0()
+	time.sleep(0.2)
+
+	eyes.Pos4_0()
+	time.sleep(0.2)
+	eyes.Pos4_1()
+	time.sleep(0.2)
+	eyes.Pos4_2()
+	time.sleep(0.2)
+	
 	
