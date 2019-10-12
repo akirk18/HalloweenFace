dbd4d8d7a10e5397af5f5c4da915484290558c64
Austin Kirk <austin@Austins-MacBook-Pro.local>
2019-10-12 17:53:13 -0500

    Added left/right and changed straight to center

--- code.py
+++ code.py
@@ -10,4 +10,16 @@ i2c = busio.I2C(board.SCL, board.SDA)
 eyes = eyes.Eyes(i2c)
 
 eyes.Brightness(3)
-eyes.Straight()
+
+while True:
+	eyes.Center()
+	time.sleep(1)
+	
+	eyes.Left()
+	time.sleep(1)
+	
+	eyes.Center()
+	time.sleep(1)
+	
+	eyes.Right()
+	time.sleep(1)
