69c4397721d4ad81ee26d06921c1e83591804e00
akirk18 <akirk18@me.com>
2019-10-22 07:21:15 -0500

    added sensor

--- code.py
+++ code.py
@@ -5,6 +5,7 @@ import busio
 import time
 import eyes
 import pygame
+import digitalio
 
 '''
 Eye Positions according to top left corner
@@ -25,33 +26,46 @@ pygame.mixer.init()
 
 i2c = busio.I2C(board.SCL, board.SDA)
 eyes = eyes.Eyes(i2c)
+sensor = digitalio.DigitalInOut(board.D4)
+sensor.direction = digitalio.Direction.INPUT
+btn = digitalio.DigitalInOut(board.D5)
+btn.direction = digitalio.Direction.INPUT
+btn.pull = digitalio.Pull.DOWN
 
 eyes.Brightness(3)
 
 pygame.mixer.music.load(file)
-#pygame.mixer.music.play()
-#pygame.event.wait()
 
 def Execute(positions, speed):
     for pos in positions:
-        pos
-        print(pos)
+        pos()
         time.sleep(speed)
 
-look_low_s2s = [eyes.Pos3_0(), eyes.Pos3_1(), eyes.Pos3_2(), eyes.Pos3_3(), eyes.Pos3_4()]
+look_low_s2s = [eyes.Pos3_0, eyes.Pos3_1, eyes.Pos3_2, eyes.Pos3_3, eyes.Pos3_4,
+                eyes.Pos3_3, eyes.Pos3_2]
 
-blink = [eyes.Blink_0(), eyes.Blink_1(), eyes.Blink_2(), eyes.Blink_3(), 
-         eyes.Blink_4(), eyes.Blink_5(), eyes.Blink_6(), eyes.Blink_7(),
-         eyes.Blink_8()]
+blink = [eyes.Blink_0, eyes.Blink_1, eyes.Blink_2, eyes.Blink_3, 
+         eyes.Blink_4, eyes.Blink_5, eyes.Blink_6, eyes.Blink_7,
+         eyes.Blink_8, eyes.Blink_7, eyes.Blink_6, eyes.Blink_5,
+         eyes.Blink_4, eyes.Blink_3, eyes.Blink_2, eyes.Blink_1,
+         eyes.Blink_0]
 
-pygame.mixer.music.play()
-print(pygame.mixer.get_busy())
+close = [eyes.Blink_0, eyes.Blink_1, eyes.Blink_2, eyes.Blink_3,
+	 eyes.Blink_4, eyes.Blink_5, eyes.Blink_6, eyes.Blink_7,
+	 eyes.Blink_8]
 
-while (not pygame.mixer.get_busy()):
-    Execute(blink, 0.1)
-    Execute(look_low_s2s, 0.3)
-    print(pygame.mixer.get_busy())
+while True:
+	if btn.value:
+		print("Button Pressed")
+		break
+	while sensor.value:
+		print("Sensor Tripped")
+		pygame.mixer.music.play()
+		while pygame.mixer.music.get_busy():
+  			Execute(blink, 0.05)
+  			Execute(look_low_s2s, 0.3)
 
+		Execute(close, 0.05)
+		eyes.Shutdown()
+print("Goodbye")
 eyes.Shutdown()
-	
-	
